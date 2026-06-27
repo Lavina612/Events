d187098e2c881af8915a6fa51b1141ae01b6bf4f
Lavina612 <lizaklokova@mail.ru>
2026-06-27 16:38:19 +0300

    Changed Tests to use Mock for IEventRepository

--- EventsRestApi/EventsRestApi.Tests/EventServiceTests.cs
+++ EventsRestApi/EventsRestApi.Tests/EventServiceTests.cs
@@ -1,248 +1,271 @@
 ﻿using EventsRestApi.Dto;
+using EventsRestApi.Interfaces;
 using EventsRestApi.Models;
 using EventsRestApi.Services;
+using Moq;
 
 namespace EventsRestApi.Tests
 {
     public class EventServiceTests
     {
-        //private readonly EventService _eventService;
-        //private readonly List<Event> allEvents = [];
-        //private readonly List<EventDto> allEventsDto = [];
+        private readonly EventService _eventService;
+        private readonly Mock<IEventRepository> _mockEventRepository;
 
-        //public EventServiceTests()
-        //{
-        //    FillTestData();
-        //    _eventService = new EventService(allEvents);
-        //}
+        private readonly List<Event> allEvents = [];
+        private readonly List<EventDto> allEventsDto = [];
 
-        //[Fact]
-        //public void GetAll_NoParamsWithMaxPageSize_ReturnAllEvents()
-        //{
-        //    var page = 1;
-        //    var pageSize = int.MaxValue;
-        //    List<EventDto> expectedEventsDto = allEventsDto;
-        //    var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
+        public EventServiceTests()
+        {
+            FillTestData();
 
-        //    var expected = new PaginatedResult<EventDto>(
-        //        expectedEventsDto,
-        //        expectedEventsDto.Count,
-        //        page,
-        //        pageSize,
-        //        totalPages);
+            _mockEventRepository = new Mock<IEventRepository>();
+            _eventService = new EventService(_mockEventRepository.Object);
+        }
 
-        //    var result = _eventService.Get(null, null, null, page, pageSize);
+        [Fact]
+        public void GetAll_NoParamsWithMaxPageSize_ReturnAllEvents()
+        {
+            var page = 1;
+            var pageSize = int.MaxValue;
+            List<EventDto> expectedEventsDto = allEventsDto;
+            var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
 
-        //    Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
-        //    Assert.Equivalent(expected, result, true);
-        //}
+            var expected = new PaginatedResult<EventDto>(
+                expectedEventsDto,
+                expectedEventsDto.Count,
+                page,
+                pageSize,
+                totalPages);
 
-        //[Fact]
-        //public void GetAll_WithTitleParam_ReturnFilteredByTitle()
-        //{
-        //    var page = 1;
-        //    var pageSize = int.MaxValue;
-        //    List<EventDto> expectedEventsDto = [allEventsDto[0]];
-        //    var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //    var expected = new PaginatedResult<EventDto>(
-        //        expectedEventsDto,
-        //        expectedEventsDto.Count,
-        //        page,
-        //        pageSize,
-        //        totalPages);
+            var result = _eventService.Get(null, null, null, page, pageSize);
 
-        //    var result = _eventService.Get("1", null, null, page, pageSize);
+            Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //    Assert.Equivalent(expected, result, true);
-        //}
+        [Fact]
+        public void GetAll_WithTitleParam_ReturnFilteredByTitle()
+        {
+            var page = 1;
+            var pageSize = int.MaxValue;
+            List<EventDto> expectedEventsDto = [allEventsDto[0]];
+            var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
 
-        //[Fact]
-        //public void GetAll_WithStartAtAndEndAtParams_ReturnFilteredBetweenDates()
-        //{
-        //    var page = 1;
-        //    var pageSize = int.MaxValue;
-        //    List<EventDto> expectedEventsDto = allEventsDto[2..4];
-        //    var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
+            var expected = new PaginatedResult<EventDto>(
+                expectedEventsDto,
+                expectedEventsDto.Count,
+                page,
+                pageSize,
+                totalPages);
 
-        //    var expected = new PaginatedResult<EventDto>(
-        //        allEventsDto[2..4],
-        //        expectedEventsDto.Count,
-        //        page,
-        //        pageSize,
-        //        totalPages);
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //    var result = _eventService.Get(null, new DateTime(2026, 06, 3, 09, 03, 00), new DateTime(2026, 06, 9, 09, 04, 00), page, pageSize);
+            var result = _eventService.Get("1", null, null, page, pageSize);
 
-        //    Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
-        //    Assert.Equivalent(expected, result, true);
-        //}
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //[Fact]
-        //public void GetAll_WithTitleAndStartAtAndEndAtParams_ReturnFilteredByTitleAndBetweenDates()
-        //{
-        //    var page = 1;
-        //    var pageSize = int.MaxValue;
-        //    List<EventDto> expectedEventsDto = [allEventsDto[2]];
-        //    var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
+        [Fact]
+        public void GetAll_WithStartAtAndEndAtParams_ReturnFilteredBetweenDates()
+        {
+            var page = 1;
+            var pageSize = int.MaxValue;
+            List<EventDto> expectedEventsDto = allEventsDto[2..4];
+            var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
 
-        //    var expected = new PaginatedResult<EventDto>(
-        //        [allEventsDto[2]],
-        //        expectedEventsDto.Count,
-        //        page,
-        //        pageSize,
-        //        totalPages);
+            var expected = new PaginatedResult<EventDto>(
+                allEventsDto[2..4],
+                expectedEventsDto.Count,
+                page,
+                pageSize,
+                totalPages);
 
-        //    var result = _eventService.Get("3", new DateTime(2026, 06, 3, 09, 03, 00), new DateTime(2026, 06, 9, 09, 04, 00), page, pageSize);
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //    Assert.Equivalent(expected, result, true);
-        //}
+            var result = _eventService.Get(null, new DateTime(2026, 06, 3, 09, 03, 00), new DateTime(2026, 06, 9, 09, 04, 00), page, pageSize);
 
-        //[Fact]
-        //public void GetAll_WithPageAndPageSize_ReturnEventsForPage()
-        //{
-        //    var page = 2;
-        //    var pageSize = 2;
-        //    List<EventDto> expectedEventsDto = allEventsDto[2..4];
-        //    var totalPages = (allEventsDto.Count + pageSize - 1) / pageSize;
+            Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //    var expected = new PaginatedResult<EventDto>(
-        //        expectedEventsDto,
-        //        allEventsDto.Count,
-        //        page,
-        //        pageSize,
-        //        totalPages);
+        [Fact]
+        public void GetAll_WithTitleAndStartAtAndEndAtParams_ReturnFilteredByTitleAndBetweenDates()
+        {
+            var page = 1;
+            var pageSize = int.MaxValue;
+            List<EventDto> expectedEventsDto = [allEventsDto[2]];
+            var totalPages = (expectedEventsDto.Count + pageSize - 1) / pageSize;
 
-        //    var result = _eventService.Get(null, null, null, page, pageSize);
+            var expected = new PaginatedResult<EventDto>(
+                [allEventsDto[2]],
+                expectedEventsDto.Count,
+                page,
+                pageSize,
+                totalPages);
 
-        //    Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
-        //    Assert.Equivalent(expected, result, true);
-        //}
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //[Fact]
-        //public void GetById_WithExistingId_ReturnEventById()
-        //{
-        //    var id = 3;
-        //    var expected = allEventsDto[2];
+            var result = _eventService.Get("3", new DateTime(2026, 06, 3, 09, 03, 00), new DateTime(2026, 06, 9, 09, 04, 00), page, pageSize);
 
-        //    var result = _eventService.GetById(id);
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //    Assert.Equivalent(expected, result, true);
-        //}
+        [Fact]
+        public void GetAll_WithPageAndPageSize_ReturnEventsForPage()
+        {
+            var page = 2;
+            var pageSize = 2;
+            List<EventDto> expectedEventsDto = allEventsDto[2..4];
+            var totalPages = (allEventsDto.Count + pageSize - 1) / pageSize;
 
-        //[Fact]
-        //public void GetById_WithNonExistentId_ReturnNull()
-        //{
-        //    var id = 0;
+            var expected = new PaginatedResult<EventDto>(
+                expectedEventsDto,
+                allEventsDto.Count,
+                page,
+                pageSize,
+                totalPages);
 
-        //    var result = _eventService.GetById(id);
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //    Assert.Null(result);
-        //}
+            var result = _eventService.Get(null, null, null, page, pageSize);
 
-        //[Fact]
-        //public void Add_CorrectEventDto_ReturnAddedEvent()
-        //{
-        //    var addingEventDto = new EventDto(
-        //        0,
-        //        $"Event 6",
-        //        $"Description 6",
-        //        new DateTime(2026, 06, 06, 09, 06, 00),
-        //        new DateTime(2026, 06, 06, 09, 06, 00).AddDays(5));
+            Assert.Equal(expected.ItemsForPage.Select(x => x.Id), result.ItemsForPage.Select(x => x.Id));
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //    var expectedEventDto = new EventDto(
-        //        6,
-        //        addingEventDto.Title,
-        //        addingEventDto.Description,
-        //        addingEventDto.StartAt,
-        //        addingEventDto.EndAt);
+        [Fact]
+        public void GetById_WithExistingId_ReturnEventById()
+        {
+            var id = 3;
+            var expected = allEventsDto[2];
 
-        //    var expectedCount = allEvents.Count() + 1;
+            _mockEventRepository.Setup(mock => mock.GetById(id)).Returns(allEvents[2]);
 
-        //    var result = _eventService.Add(addingEventDto);
+            var result = _eventService.GetById(id);
 
-        //    Assert.Equivalent(expectedEventDto, result, true);
-        //    Assert.Equal(expectedCount, allEvents.Count());
-        //}
+            Assert.Equivalent(expected, result, true);
+        }
 
-        //[Fact]
-        //public void Update_WithExistingId_ReturnTrueAndEventWasUpdated()
-        //{
-        //    var id = 1;
+        [Fact]
+        public void GetById_WithNonExistentId_ReturnNull()
+        {
+            var id = 0;
 
-        //    var updatingEventDto = new EventDto(
-        //        1,
-        //        $"Event New",
-        //        $"Description 1",
-        //        new DateTime(2026, 06, 01, 09, 01, 00),
-        //        new DateTime(2026, 06, 01, 09, 01, 00).AddDays(5));
+            _mockEventRepository.Setup(mock => mock.GetById(id)).Returns((Event?)null);
 
-        //    var result = _eventService.Update(id, updatingEventDto);
-        //    var actualEvent = EventMapper.MapToEventDto(allEvents[0]);
+            var result = _eventService.GetById(id);
 
-        //    Assert.True(result);
-        //    Assert.Equivalent(updatingEventDto, actualEvent, true);
-        //}
+            Assert.Null(result);
+        }
 
-        //[Fact]
-        //public void Update_WithNonExistentId_ReturnFalse()
-        //{
-        //    var id = 0;
+        [Fact]
+        public void Add_CorrectEventDto_EventIdWasChanged()
+        {
+            var addingEventDto = new EventDto(
+                0,
+                $"Event 6",
+                $"Description 6",
+                new DateTime(2026, 06, 06, 09, 06, 00),
+                new DateTime(2026, 06, 06, 09, 06, 00).AddDays(5));
 
-        //    var updatingEventDto = new EventDto(
-        //        0,
-        //        $"Event New",
-        //        $"Description 1",
-        //        new DateTime(2026, 06, 01, 09, 01, 00),
-        //        new DateTime(2026, 06, 01, 09, 01, 00).AddDays(5));
+            var expectedEventDto = new EventDto(
+                6,
+                addingEventDto.Title,
+                addingEventDto.Description,
+                addingEventDto.StartAt,
+                addingEventDto.EndAt);
 
-        //    var result = _eventService.Update(id, updatingEventDto);
+            _mockEventRepository.Setup(mock => mock.Get()).Returns(allEvents);
 
-        //    Assert.False(result);
-        //}
+            var result = _eventService.Add(addingEventDto);
 
-        //[Fact]
-        //public void Delete_WithExistingId_ReturnTrueAndEventWasDeleted()
-        //{
-        //    var id = allEvents.Last().Id;
-        //    var expectedEventsCount = allEvents.Count - 1;
+            Assert.Equivalent(expectedEventDto, result, true);
+        }
 
-        //    var result = _eventService.Delete(id);
+        [Fact]
+        public void Update_WithExistingId_ReturnTrue()
+        {
+            var id = 1;
 
-        //    Assert.True(result);
-        //    Assert.Equal(expectedEventsCount, allEvents.Count);
-        //}
+            var updatingEventDto = new EventDto(
+                1,
+                $"Event New",
+                $"Description 1",
+                new DateTime(2026, 06, 01, 09, 01, 00),
+                new DateTime(2026, 06, 01, 09, 01, 00).AddDays(5));
 
-        //[Fact]
-        //public void Delete_WithNonExistentId_ReturnFalse()
-        //{
-        //    var id = 0;
+            _mockEventRepository.Setup(mock => mock.Update(id, It.IsAny<Event>())).Returns(true);
 
-        //    var result = _eventService.Delete(id);
+            var result = _eventService.Update(id, updatingEventDto);
 
-        //    Assert.False(result);
-        //}
+            Assert.True(result);
+        }
 
-        //private void FillTestData()
-        //{
-        //    var title = "Event";
-        //    var description = "Description";
-        //    var eventCount = 5;
-        //    Event currentEvent;
+        [Fact]
+        public void Update_WithNonExistentId_ReturnFalse()
+        {
+            var id = 0;
 
-        //    for (int i = 1; i <= eventCount; i++)
-        //    {
-        //        currentEvent = new Event(
-        //            i,
-        //            $"{title} {i}",
-        //            $"{description} {i}",
-        //            new DateTime(2026, 06, i % 28, 09, i % 60, 00),
-        //            new DateTime(2026, 06, i % 28, 09, i % 60, 00).AddDays(eventCount));
+            var updatingEventDto = new EventDto(
+                0,
+                $"Event New",
+                $"Description 1",
+                new DateTime(2026, 06, 01, 09, 01, 00),
+                new DateTime(2026, 06, 01, 09, 01, 00).AddDays(5));
 
-        //        allEvents.Add(currentEvent);
+            _mockEventRepository.Setup(mock => mock.Update(id, It.IsAny<Event>())).Returns(false);
 
-        //        allEventsDto.Add(EventMapper.MapToEventDto(currentEvent));
-        //    }
-        //}
+            var result = _eventService.Update(id, updatingEventDto);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Delete_WithExistingId_ReturnTrue()
+        {
+            var id = allEvents.Last().Id;
+
+            _mockEventRepository.Setup(mock => mock.Delete(id)).Returns(true);
+
+            var result = _eventService.Delete(id);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Delete_WithNonExistentId_ReturnFalse()
+        {
+            var id = 0;
+
+            _mockEventRepository.Setup(mock => mock.Delete(id)).Returns(false);
+
+            var result = _eventService.Delete(id);
+
+            Assert.False(result);
+        }
+
+        private void FillTestData()
+        {
+            var title = "Event";
+            var description = "Description";
+            var eventCount = 5;
+            Event currentEvent;
+
+            for (int i = 1; i <= eventCount; i++)
+            {
+                currentEvent = new Event(
+                    i,
+                    $"{title} {i}",
+                    $"{description} {i}",
+                    new DateTime(2026, 06, i % 28, 09, i % 60, 00),
+                    new DateTime(2026, 06, i % 28, 09, i % 60, 00).AddDays(eventCount));
+
+                allEvents.Add(currentEvent);
+
+                allEventsDto.Add(EventMapper.MapToEventDto(currentEvent));
+            }
+        }
     }
 }
