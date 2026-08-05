a1f6ea4d9825ecbc06199d438eaf71a293d69250
Lavina612 <lizaklokova@mail.ru>
2026-08-05 23:23:54 +0300

    Removed empty constructor for Event

--- EventsRestApi/EventsRestApi/Models/Event.cs
+++ EventsRestApi/EventsRestApi/Models/Event.cs
@@ -4,15 +4,13 @@
     {
         public Guid Id { get; set; }
 
-        public string Title { get; set; } = string.Empty;
+        public string Title { get; set; }
 
         public string? Description { get; set; }
 
         public DateTime StartAt { get; set; }
 
         public DateTime EndAt { get; set; }
-
-        public Event() { }
 
         public Event(
             Guid id,
