9bb878087007c354a130d7a86664e581e763a77f
Lavina612 <lizaklokova@mail.ru>
2026-08-20 15:07:24 +0300

    Refactroing GlobalExceptionHandlingMiddleware

--- EventsRestApi/EventsRestApi/Exceptions/NoAvailableSeatsException.cs
+++ EventsRestApi/EventsRestApi/Exceptions/NoAvailableSeatsException.cs
@@ -9,7 +9,7 @@
         public int AvailableSeats { get; }
 
         public NoAvailableSeatsException(Guid eventId, int requestedSeats, int availableSeats) 
-            : base($"На мероприятии с Id : {eventId} недостаточно мест. " +
+            : base($"На мероприятии с Id: {eventId} недостаточно мест. " +
                   $"Запрошено мест: {requestedSeats}, доступно мест: {availableSeats}.") 
         {
             EventId = eventId;
